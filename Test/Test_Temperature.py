6bd3a6c5c2d0cebf6e2a6493fb285db2a69b6006
Adan Martinez <adanarturomartinez@gmail.com>
2026-01-31 21:29:22 -0600

    Adding changes for Test Temparature code, adding zero division error

---
+++ Test/Test_Temperature.py
@@ -0,0 +1,52 @@
+import os
+from random import randint
+
+def limpiar_terminal():
+    # 'nt' es para Windows, 'posix' para Mac y Linux
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+# Llama a la función justo antes de tus pruebas
+limpiar_terminal()
+def calculate_engine_health(temp, pressure):
+    # La temperatura de operación segura es entre 150 y 250 grados
+    # La presión segura es entre 30 y 50 psi
+    
+    if 150 <= temp and temp <= 250:
+        
+        if 30 <= pressure and pressure <= 50:
+            status = "Optimal Temperature and Optimal pressure"
+            print('Temperature:',temp,'Pressure:',pressure)
+        else:
+            status = "Optimal Temperature and High Pressure Alert"
+            print('Temperature:',temp,'Pressure:',pressure)
+    else:
+        if 30 <= pressure and pressure <= 50:
+            status = "Critical Temperature and Optimal pressure"
+            print('Temperature:',temp,'Pressure:',pressure)
+        else:
+            status = "Critical Temperature and High Pressure Alert"
+            print('Temperature:',temp,'Pressure:',pressure)
+    try:
+        health_score = (temp / pressure) * 100
+
+    except ZeroDivisionError:
+        return ("Error pression is:"), 0.0
+    
+
+    return status, health_score
+
+# Ejemplo de uso que falla:
+print(calculate_engine_health(150, 30))
+print(calculate_engine_health(149, 29))
+print(calculate_engine_health(151, 31))
+
+print(calculate_engine_health(250, 50))
+print(calculate_engine_health(249, 49))
+print(calculate_engine_health(251, 51))
+
+print(calculate_engine_health(251, 0))
+
+print(calculate_engine_health(randint(150, 250), randint(30, 50)))
